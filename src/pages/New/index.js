96e1c5f7be0f438ac6b16dfe0a8002b3034df701
MEGA\andre.soares <andre.soares@mega.com.br>
2019-10-04 19:48:18 -0300

    Cadastro de sposts funcionando

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,27 +1,46 @@
 import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 
 import camera from '../../assets/camera.svg';
 
-import './styles.css'
+import './styles.css';
 
-export default function New() {
-    const { thumbnail, setThumbnail} = useState(null);
-    const { company, setCompany} = useState('');
-    const { techs, setTechs} = useState('');
-    const { price, setPrice} = useState('');
+export default function New( { history } ) {
+    const [ thumbnail, setThumbnail ] = useState(null);
+    const [ company, setCompany ] = useState('');
+    const [ techs, setTechs ] = useState('');
+    const [ price, setPrice ] = useState('');
 
     const preview = useMemo(() => {
         return thumbnail ? URL.createObjectURL(thumbnail) : null;
-    }, [thumbnail]);
+    }, [thumbnail])
 
-    function handleSubmit() {
+    async function handleSubmit(event) {
+      event.preventDefault();
+      
+      const data = new FormData();
+      const user_id = localStorage.getItem('user');
 
+      data.append('thumbnail', thumbnail);
+      data.append('company', company);
+      data.append('techs', techs);
+      data.append('price', price);
+
+      await api.post('/spots', data, {
+        headers: { user_id }
+      })
+
+      history.push('/dashboard');
     }
 
     return (
         <form onSubmit={handleSubmit}>       
             <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }}>
-                <input type="file" />                
+                <input 
+                  type="file"
+                  onChange={event => setThumbnail(event.target.files[0])}
+                  className={thumbnail ? 'has-thumbnail' : ''}
+                />               
                 <img src={camera} alt="Select img"/>
             </label>
 
