6cbacd929baa91d9edac11e9559ce1b4d3dc90a5
andre09soares <andre09soares@gmail.com>
2019-10-08 23:14:36 -0300

    ajuste de mensagem

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -55,8 +55,8 @@ export default function Dashboard() {
                     <li key={request._id}>
                         <p>
                             <strong>{request.user.email}</strong> está solicitando uma reserva em
-                            <strong>{request.spot.company}</strong> para a data:
-                            <strong>{request.date}</strong>
+                            <strong> {request.spot.company}</strong> para a data:
+                            <strong> {request.date}</strong>
                         </p>
                         <button className="accept" onClick={ () => handleAccept(request._id) }> ACEITAR</button>
                         <button className="reject" onClick={ () => handleReject(request._id) }>REJEITAR</button>
